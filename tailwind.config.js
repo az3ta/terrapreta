8f56229e428611c9f3abaa5a15b9871b847a9982
andreazaccuri <24527778+andreazaccuri@users.noreply.github.com>
2021-10-05 16:40:01 +0200

    new tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				'goccia-green': '#567762',
+				'goccia-gold': '#ad966d',
+				'goccia-black': '#292728',
+			},
+			fontFamily: {
+				inter: [
+					'Inter',
+					'ui-sans-serif',
+					'system-ui',
+					'Helvetica',
+					'Arial',
+					'sans-serif',
+				],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
